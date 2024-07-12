b8b3f0f80a37f114348114e9df6b86b28d780c92
stinobook <stijn.schepers@me.com>
2024-07-12 12:31:05 +0200

    Added post-element- cleanup homepage posts

---
+++ src/components/post.ts
@@ -0,0 +1,132 @@
+import { LiteElement, customElement, html, property } from '@vandeurenglenn/lite'
+import { StyleList, css } from '@vandeurenglenn/lite/element'
+
+@customElement('post-element')
+export class PostElement extends LiteElement {
+  @property()
+  accessor image
+
+  @property()
+  accessor headline
+
+  @property()
+  accessor subline
+
+  @property()
+  accessor content
+
+  onChange(propertyKey: string, value: any): void {
+    if (propertyKey === 'image') {
+      if (value) this.setAttribute('has-image', '')
+      else this.removeAttribute('has-image')
+    }
+  }
+
+  static styles?: StyleList = [
+    css`
+      :host {
+        border-radius: var(--md-sys-shape-corner-extra-large);
+        background: var(--md-sys-color-surface-variant);
+        color: var(--md-sys-color-on-surface-variant);
+      }
+      :host * {
+        box-sizing: border-box;
+      }
+      .card {
+        display: flex;
+        margin: 1rem auto;
+        box-shadow: 0 3px 7px -1px rgba(#000, .1);
+        line-height: 1.4;
+        justify-content: center;
+        flex-wrap: wrap;
+        flex-direction: var(--flex-direction);
+        align-items: stretch;
+        align-content: stretch;
+        gap: 12px;
+        padding: 12px;
+      }
+      .img {
+        flex: 1 1 30%;
+        min-height:150px;
+        align-items: center;
+        display: flex;
+      }
+      img {
+        width: 100%;
+        max-height:300px;
+        object-fit: contain;
+        border-radius: 5px;
+      }
+      .content {
+        padding: 1rem;
+        position: relative;
+        flex: 3 0 65%;
+      }
+      h1 {
+        line-height: 1;
+        margin: 0;
+        font-size: 1.7rem;
+      }
+      h2 {
+        font-size: 1rem;
+        font-weight: 300;
+        margin-top: 5px;
+      }
+      p {
+        position: relative;
+        margin-bottom: 12px;
+      }
+      custom-icon {
+        --custom-icon-color: var(--md-sys-color-on-primary);
+      }
+      custom-button {
+        margin-bottom: 12px;
+      }
+      @media (max-width: 640px) {
+        .card {
+          flex-direction: column;
+        }
+        img {
+          margin: 0 auto;
+          width: auto;
+        }
+      }
+    `
+  ]
+
+  _renderImage() {
+    if (!this.image) return ''
+    return html` <img loading="lazy" src=${this.image} /> `
+  }
+
+  _renderHeadline() {
+    if (!this.headline) return ''
+    return html`<h1>${this.headline}</h1>`
+  }
+
+  _renderSubline() {
+    if (!this.subline) return ''
+    return html`<h2>${this.subline}</h2>`
+  }
+
+  _renderContent() {
+    if (!this.content) return ''
+    return html`<p>${this.content}</p>`
+  }
+
+  render() {
+    return html`
+    <div class="card">
+      <div class="img">
+        ${this._renderImage()}
+      </div>
+      <div class="content">
+        ${this._renderHeadline()} 
+        ${this._renderSubline()}
+        ${this._renderContent()}
+       <slot></slot>
+      </div>
+    </div>
+    `
+  }
+}
--- src/views/home.ts
+++ src/views/home.ts
@@ -2,6 +2,7 @@ import { html, css, LiteElement, query } from '@vandeurenglenn/lite'
 import { customElement } from 'lit/decorators.js'
 import '@vandeurenglenn/flex-elements/container.js'
 import '@vandeurenglenn/lite-elements/button.js'
+import '../components/post.js'
 
 @customElement('home-view')
 export class HomeView extends LiteElement {
@@ -20,17 +21,18 @@ export class HomeView extends LiteElement {
         border-radius: var(--md-sys-shape-corner-extra-large);
         box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.5) inset;
       }
-      img{
-        max-width:100%;
-        padding: 25px 0;
-        object-fit:contain;
-      }
       flex-container {
         flex-direction: column;
+        max-width: 1024px;
+        gap:12px;
+        height:fit-content;
         margin: 0 auto;
       }
-      custom-icon {
-        --custom-icon-color: var(--md-sys-color-on-primary);
+      post-element:nth-of-type(even) {
+        --flex-direction: row;
+      }
+      post-element:nth-of-type(odd) {
+        --flex-direction: row-reverse;
       }
     `
   ]
@@ -38,13 +40,18 @@ export class HomeView extends LiteElement {
   render() {
     return html`
     <flex-container>
-      <h2>Welkom!</h2>
-      <p>
-      Onze hondenschool bestaat meer dan 25 jaar en elke woensdag- en vrijdagavond geven wij gehoorzaamheidstrainingen.
+      <post-element
+      image="../img/canvas.svg"
+      headline="Welkom!"
+      content="Onze hondenschool bestaat meer dan 25 jaar en elke woensdag- en vrijdagavond geven wij gehoorzaamheidstrainingen.
       Doch merken wij dat er daarnaast nog enorm veel vragen zijn en dit is de reden waarom wij onderstaande thema avonden organiseren.
-      Toekomstige hondeneigenaars of mensen die reeds een hond hebben dienen nog beter geïnformeerd te worden.
-      </p>
-      <h3>6 thema-avonden</h3>
+      Toekomstige hondeneigenaars of mensen die reeds een hond hebben dienen nog beter geïnformeerd te worden."
+      ></post-element>
+      <post-element
+      image="../img/thema-avonden.webp"
+      headline="6 Thema-avonden"
+      subline="Informatieavond over diverse onderwerpen"
+      .content=${html`
       <ol>
         <li>Aanschaf van een hond – rassen – fokker : maandag 1 juli 2024 19u30</li>
         <li>Speeltjes maken voor honden: donderdag 22 augustus 2024 19u30</li>
@@ -61,25 +68,33 @@ export class HomeView extends LiteElement {
       <custom-button type="filled" label="Inschrijven">
       <span slot="icon"><custom-icon icon="pen"></custom-icon></span>
       </custom-button>
-
-      <h3>Trooper</h3>
+      `}
+      ></post-element>
+      <post-element
+      image="../img/QRtrooper.webp"
+      headline="Trooper"
+      subline="Gratis steun voor onze hondenschool dankzij online-shopping"
+      .content=${html`
       <p>Wens je onze hondenschool een duwtje in de rug geven? Shop dan online via Trooper en steun gratis onze hondenschool!
       Dankzij Trooper komt de commissie die anders naar advertentiebureau's gaan naar onze VZW, en jij betaalt er niets extra voor!
       </p>
       <custom-button type="filled" label="Ik troop mee!" @click=${() => window.open('https://www.trooper.be/nl/trooperverenigingen/hondenschoolpoho', '_blank')}>
       <span slot="icon"><custom-icon icon="check"></custom-icon></span>
       </custom-button>
-
-      <h3>Instructeur worden</h3>
+      `}
+      ></post-element>
+      <post-element
+      image="../img/logo.png"
+      headline="Instructeur worden?"
+      subline="Bel 0477/523983 (Joke De Swaef)"
+      .content=${html`
       <p>Hondenschool PoHo zijn deuren staan altijd open voor kandidaat-instructeurs (m/v/x)
       die zeer goed kunnen omgaan met zowel mens als dier en die als hun hobby hondeneigenaars
       willen wegwijs maken in de opvoeding en het positief trainen van de hond(en).
       Begeleiding, opvolging en bijscholingen op regelmatige basis zijn verzekerd.</p>
       <p>Indien interesse neem dan contact op met de leden van het bestuur.</p>
-      <p>Bel 0477/523983 (Joke De Swaef)</p>
-
-
-    <img src="/img/logo.png" />
+      `}
+      ></post-element>
     </flex-container>
     `
   }
